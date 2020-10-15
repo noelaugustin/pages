d45cc5c482f8efa206c6be8705372ffb4444ca45
noelaugustin <noel.noelaugustin@gmail.com>
2020-10-15 08:09:41 +0100

    Scaling

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -3,6 +3,10 @@ set -ex
 
 kubectl apply -f deployment/pages-namespace.yaml
 kubectl apply -f deployment/pages-config.yaml
+
+kubectl apply -f deployment/mysql-pv.yaml
+kubectl apply -f deployment/mysql-pvc.yaml
+
 kubectl apply -f deployment/pages-service.yaml
 kubectl apply -f deployment/pages-deployment.yaml
 
